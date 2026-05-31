6e74c15dbd9df9907329933129d6e6fbde4097dc
Zach Hedeen <hedeenza@gmail.com>
2026-05-31 03:02:49 -0400

    removed text summary framework, moved to reading text from external file

--- src/main.rs
+++ src/main.rs
@@ -1,193 +1,6 @@
 use regex::Regex;
 use std::collections::HashSet;
-use std::collections::BTreeSet;
-use ndarray::{Array1, Array2};
-use unicode_segmentation::UnicodeSegmentation;
 
-// Summarize Text
-pub fn summarize(text: &str, stop_words: &[&str], num_sentence: usize) -> String {
-  let sentences = text.unicode_sentences().collect::<Vec<&str>>();
-  if num_sentence >= sentences.len() {
-    return text.to_string();
-  }
-  let mut sentences_and_words = vec![];
-  sentences.iter().for_each(|&sentence| {
-    let words = split_into_words(sentence);
-    sentences_and_words.push(words);
-  });
-  let matrix = build_similarity_matrix(&sentences_and_words, stop_words);
-  let ranks = calculate_sentence_rank(&matrix);
-  let mut sorted_ranks = ranks.clone();
-  sorted_ranks.sort_by(|a, b| b.partial_cmp(a).unwrap());
-  let least_rank = sorted_ranks[num_sentence + 1];
-  let mut result: Vec<&str> = vec![];
-  let mut included_count = 0;
-  for i in 0..sentences.len() {
-    if ranks[i] >= least_rank {
-      included_count = included_count + 1;
-      result.push(sentences[i]);
-    }
-    if included_count == num_sentence {
-      break;
-    }
-  }
-  result.join("")
-}
-
-fn get_all_words_lc<'a>(sentence1: &[&'a str], sentence2: &[&'a str]) -> BTreeSet<String> {
-  let mut all_words: BTreeSet<String> = BTreeSet::new();
-
-  sentence1.iter().for_each(|w| {
-    all_words.insert(w.to_lowercase());
-  });
-
-  sentence2.iter().for_each(|w| {
-    all_words.insert(w.to_lowercase());
-  });
-  return all_words;
-}
-
-/*
-Retrieve a sentence vector based on the frequency of words that appears in the all_words_lc set.
-all_words_lc should be a sorted set of lower cased words
-The size of the resulting vector is the same as the all_words_lc set
-stop_words are skipped
-*/
-fn get_sentence_vector(sentence: &[&str], all_words_lc: &BTreeSet<String>, stop_words: &[&str]) -> Vec<usize> {
-  let mut vector: Vec<usize> = vec![0; all_words_lc.len()];
-  for word in sentence {
-    let word_lc = word.to_lowercase();
-    if !stop_words.contains(&word_lc.as_str()) {
-      let index = all_words_lc.iter().position(|x| x.eq(&word_lc)).unwrap();
-      vector[index] += 1;
-    }
-  }
-  return vector;
-}
-
-/*
-Calculates the cosine distance between two vectors
-Refer to [YouTube](https://www.youtube.com/watch?v=3X0wLRwU_Ws)
-*/
-fn cosine_distance(vec1: &Vec<usize>, vec2: &Vec<usize>) -> f64 {
-  let dot_product = dot_product(vec1, vec2);
-  let root_sum_square1 = root_sum_square(vec1);
-  let root_sum_square2 = root_sum_square(vec2);
-  return dot_product as f64 / (root_sum_square1 * root_sum_square2);
-}
-
-fn root_sum_square(vec: &Vec<usize>) -> f64 {
-  let mut sum_square = 0;
-  for i in 0..vec.len() {
-    sum_square += vec[i] * vec[i];
-  }
-  (sum_square as f64).sqrt()
-}
-
-fn dot_product(vec1: &Vec<usize>, vec2: &Vec<usize>) -> usize {
-  let delta = vec1.len() - vec2.len();
-  let shortest_vec = match delta {
-    d if d < 0 => vec1,
-    d if d > 0 => vec2,
-    _ => vec1
-  };
-  let mut dot_product = 0;
-  for i in 0..shortest_vec.len() {
-    dot_product += vec1[i] * vec2[i];
-  }
-  dot_product
-}
-
-fn sentence_similarity(s1: &[&str], s2: &[&str], stop_words: &[&str]) -> f64 {
-  let all_words = get_all_words_lc(s1, s2);
-  let v1 = get_sentence_vector(s1, &all_words, stop_words);
-  let v2 = get_sentence_vector(s2, &all_words, stop_words);
-  1.0 - cosine_distance(&v1, &v2)
-}
-
-/*
-Calculate a similarity matrix for the given sentences.
-Returns a 2-D array M_i,j such that for all 'j', sum(i, M_i,j) = 1
-We take a leap of faith here and assume that cosine similarity is similar to the probability
-that a sentence is important for summarization
-*/
-fn build_similarity_matrix(sentences: &Vec<Vec<&str>>, stop_words: &[&str]) -> Array2<f64> {
-  let len = sentences.len();
-  let mut matrix = Array2::<f64>::zeros((len, len));
-  let mut sum_column: Vec<f64> = vec![0.0; len];
-  for i in 0..len {
-    for j in 0..len {
-      if i == j {
-        continue;
-      }
-      matrix[[i, j]] = sentence_similarity(sentences[i].as_slice(), sentences[j].as_slice(), stop_words);
-    }
-  }
-  // at this point we have the cosine similarity of each sentence.
-  // take a leap of faith and assume that the cosine similarity is the probability that a sentence
-  // is important for summarization.
-  // We do this by normalizing the matrix along the column. The column values should add up to 1.
-  for j in 0..len {
-    let mut sum: f64 = 0.0;
-    for i in 0..len {
-      if i == j {
-        continue;
-      }
-      sum += matrix[[i, j]];
-    }
-    sum_column[j] = sum;
-  }
-  for i in 0..len {
-    for j in 0..len {
-      if i == j {
-        continue;
-      }
-      matrix[[i, j]] = matrix[[i, j]] / sum_column[j];
-    }
-  }
-  matrix
-}
-
-/*
-Calculate a sentence rank similar to a page rank.
-Please refer to [PageRank](https://en.wikipedia.org/wiki/PageRank) for more details.
-*/
-fn calculate_sentence_rank(similarity_matrix: &Array2<f64>) -> Vec<f64> {
-  let num_sentence = similarity_matrix.shape()[1];
-  let threshold = 0.001;
-  // Initialize a vector with the same value 1/number of sentences. Uniformly distributed across
-  // all sentences. NOTE: perhaps we can make some sentences more important than the rest?
-  let initial_vector: Vec<f64> = vec![1.0 / num_sentence as f64; num_sentence];
-  let mut result = Array1::from(initial_vector);
-  let mut prev_result = result.clone();
-  let damping_factor = 0.85;
-  let initial_m = damping_factor * similarity_matrix + (1.0 - damping_factor) / num_sentence as f64;
-  loop {
-    result = initial_m.dot(&result);
-    let delta = &result - &prev_result;
-    let mut converged = true;
-    for i in 0..delta.len() {
-      if delta[i] > threshold {
-        converged = false;
-        break;
-      }
-    }
-    if converged {
-      break;
-    }
-    prev_result = result.clone();
-  }
-  result.into_raw_vec()
-}
-
-fn split_into_words(sentence: &str) -> Vec<&str> {
-  let mut result = vec![];
-  let words = sentence.unicode_words();
-  for word in words {
-    result.push(word);
-  }
-  result
-}
 
 fn main() {
 
@@ -196,7 +9,18 @@ fn main() {
     ONE BATTLE AFTER ANOTHER The mood music has changed dramatically since Trump declared in a Truth Social post in April 2025 that his tariffs would make China realise that the *days of ripping off* the United States were over. Those levies prompted Beijing to restrict exports of rare earths, brutally exposing the West's dependency on elements vital to the manufacturing of everything from electric cars to weapons, and eventually led to Trump and Xi's ‌fragile truce. Since then, Trump has faced countless other battles: capturing Venezuela's leader, threatening to annex fellow NATO member Greenland and waging a war on Iran that has plunged the Middle East into chaos and stoked a global energy crisis. More than 60% of Americans disapprove of his Iran war, according to a Reuters/Ipsos survey last month. Now, Trump wants China to convince Tehran to make a deal with Washington to end the conflict. China maintains ties with Iran and remains a major consumer of its oil exports. Matt Pottinger, who served as deputy national security advisor during Trump's first term, told a forum in Taipei last week that while China would like to see an outcome that weakens American power it is not immune to the economic cost of a protracted conflict. But Beijing will want something in return, and top of Xi's agenda is Taiwan, the democratically governed island claimed by China. While some fear a bargain that could embolden China to take Taiwan by force, even a nuanced change in Washington's wording would raise anxiety about the commitment of Taipei's most important backer that would reverberate across other U.S. allies in Asia. Wu Xinbo, a professor at Fudan University in Shanghai who serves on the policy advisory board of China's foreign ministry, said Trump should make clear that he *won't support independence or take actions that encourage a separatist political agenda*.
     'SUPERFICIAL CEASEFIRE' China also wants the Trump administration to commit to not taking future retaliatory trade action such as technology export controls, and to roll back existing controls on chipmaking equipment and advanced memory chips, people briefed on the talks said. And since last October, Beijing has been expanding its own economic leverage, such as enacting laws to punish foreign entities that shift supply chains away from China and tightening its rare earth licensing regime. A majority of Americans (53 percent) now say the United States should undertake friendly cooperation and engagement with China, up from 40 percent in 2024, according to a survey by the Chicago Council on Global Affairs published in October. So just keeping relations on an even keel and extending the trade war truce could be enough for Trump to claim a win. That leaves the main outcome likely to be *a superficial ceasefire that is largely to China's advantage,* said Scott Kennedy of the Center for Strategic and International Studies think tank in Washington.*";
 
-    let summary = summarize(text, &STOP_WORDS, 10);
+    // Read file
+    let input_file = File::open(&args.input).unwrap();
+    let input_reader = BufReader::new(input_file);
+    let mut text = String::new();
+
+    // Push file lines to string
+    for line in input_reader.lines() {
+        match line {
+            Ok(line) => { text.push_str(&line); }
+            Err(err) => { println!("[ ERROR ] : {}", err); }
+        }
+    }
 
     // Create an empty string to contain the "linked" text.
     let mut linked_text = String::new();
@@ -579,9 +403,9 @@ fn main() {
         linked_text = cleaned_text;
     }
 
-    println!("SUMMARY:\n{}\n", linked_text);
+    println!("Linked Text:\n{}\n", linked_text);
 
-    println!("{}", text);
+    println!("Original Text:\n{}", text);
 
 }
 
