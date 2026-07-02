eccd55eccdd91a84f0e046c4e23d88b632d7f05c
Zach Hedeen <hedeenza@gmail.com>
2026-07-02 15:54:05 -0400

    imported boilerplate dynamic window pattern, altered to match function of previous link_entities function, included lines to handle 'core entity' linking without trailing punctuation, included lines to handle generation of variants to prevent repeat linking

--- src/link_entities.rs
+++ src/link_entities.rs
@@ -1,4 +1,82 @@
+use regex::Regex;
 use std::collections::HashSet;
+
+pub fn link_entities_new(text_vector: &[String]) -> String {
+    // Set the capital word regex pattern
+    let capital_word = Regex::new(r"^[A-Z]").unwrap();
+
+    // Set a vector of allowable filler words for Pointer 2 to move through
+    let allowable_filler = vec!["of", "the", "from"];
+
+    // Create a string to hold the linked text
+    let mut linked_text = String::new();
+
+    // Create a new vector to hold
+    let mut previously_linked: Vec<String> = Vec::new();
+
+    // Set the initial Pointer 1 index to the beginning
+    let mut pointer1_index = 0;
+
+    // While the Pointer 1 index is less than the length of the content vector...
+    while pointer1_index < text_vector.len() {
+        // If Pointer 1 hits a line with content in it...
+        if capital_word.is_match(&text_vector[pointer1_index]) {
+            // Create a new string to hold the entire entity to link
+            let mut entity: String = String::new();
+            // Set Pointer 2 to where Pointer 1 is
+            let mut pointer2_index = pointer1_index;
+            // While the Pointer 2 index is less than the length of the content vector...
+            while pointer2_index < text_vector.len() {
+                // If Pointer 2 hits a blank line...
+                if !capital_word.is_match(&text_vector[pointer2_index]) && !allowable_filler.contains(&text_vector[pointer2_index].as_str()) {
+                    // Push each line between Pointer 1 and Pointer 2 to a Vector
+                    for word in &text_vector[pointer1_index..pointer2_index] {
+                        entity.push_str(&word);
+                    }
+
+                    // Manipulate the string to account for final commas, periods, etc.
+                    if entity.ends_with(".") {
+                        let stripped_entity = entity.replace(".", "");
+                        let linked_entity = format!("[[{}]].", stripped_entity);
+                        linked_text.push_str(&(linked_entity + " "));
+                    } else if entity.ends_with(",") {
+                        let stripped_entity = entity.replace(",", "");
+                        let linked_entity = format!("[[{}]],", stripped_entity);
+                        linked_text.push_str(&(linked_entity + " "));
+                    } else if entity.ends_with("'s") {
+                        let stripped_entity = entity.replace("'s", "");
+                        let linked_entity = format!("[[{}]]'s", stripped_entity);
+                        linked_text.push_str(&(linked_entity + " "));
+                    } else {
+                        let linked_entity = format!("[[{}]]", entity);
+                        linked_text.push_str(&(linked_entity + " "));
+                    }
+                    
+                    // Create the variants of the Entity and add those to "previously_linked"
+                    let strip_characters = vec![".", ",", "'s"];
+                    for character in strip_characters {
+                        let variant = entity.replace(character, "");
+                        previously_linked.push(variant)
+                    }
+
+                    // Move Pointer 1 up to Pointer 2
+                    pointer1_index = pointer2_index;
+                    break;
+                }
+                // Increment Pointer 2 by One
+                pointer2_index += 1;
+            }
+            // Push the tuple vector to the output vector
+            previously_linked.push(entity);
+        } else {
+            // If it is not an entity, append to the linked text
+            linked_text.push_str(&text_vector[pointer1_index]);
+        }
+        // Increment Pointer 1 by One
+        pointer1_index += 1;
+    }
+    linked_text
+}
 
 pub fn link_entities(
     text_vector: Vec<&str>,
